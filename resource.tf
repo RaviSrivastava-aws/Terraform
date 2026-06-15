8cedb31b33c420ba0709118ac9f7fff1e6e25a9c
Ravi-dev-reput <ravi.s@reput.co.in>
2026-06-15 16:44:09 +0530

    s3 bucket added

--- resource.tf
+++ resource.tf
@@ -19,3 +19,12 @@ resource "aws_instance" "test_ec2" {
     Name = "test-ec2"
   }
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-s3"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}

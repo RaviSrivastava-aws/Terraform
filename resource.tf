937b6c0c4efbfa4999f50ce3c62aa71b7fa47aaa
Ravi-dev-reput <ravi.s@reput.co.in>
2026-06-16 17:16:06 +0530

    fix variables

--- resource.tf
+++ resource.tf
@@ -16,12 +16,10 @@ module "ec2" {
 
   ami_id        = var.ami_id
   instance_type = var.instance_type
-  instance_name = "test-ec2"
 }
 
 module "s3" {
   source = "./modules/s3"
 
   bucket_name = var.bucket_name
-  environment = "production"
 }

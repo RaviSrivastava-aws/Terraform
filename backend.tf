90bede63485a449165f6ba8f4e634d1c58ec487b
Ravi-dev-reput <ravi.s@reput.co.in>
2026-06-16 13:34:00 +0530

    Add Terraform workflow debugging

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-tf-test-s3bucet-for-dev"
-    key            = "terraform/github-actions/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
+    bucket       = "my-tf-test-s3bucet-for-dev"
+    key          = "terraform/github-actions/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }

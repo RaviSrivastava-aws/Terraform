4657e481355d2217f1dc9ca86fd5b6309187a273
Ravi-dev-reput <ravi.s@reput.co.in>
2026-06-16 17:09:45 +0530

    added modules

--- resource.tf
+++ resource.tf
@@ -4,86 +4,24 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.7"
-    }
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
 }
 
-resource "aws_instance" "test_ec2" {
-  ami           = "ami-0f918f7e67a3323f0"
-  instance_type = "t2.micro"
+module "ec2" {
+  source = "./modules/ec2"
 
-  tags = {
-    Name = "test-ec2"
-  }
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  instance_name = "test-ec2"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-s3bucket-for-productions"
+module "s3" {
+  source = "./modules/s3"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "production"
-  }
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "example" {
-  name               = "lambda_execution_role_test"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.example.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-data "archive_file" "example" {
-  type        = "zip"
-  source_file = "${path.module}/lambda/index.js"
-  output_path = "${path.module}/lambda/fix.zip"
-}
-
-resource "aws_lambda_function" "example" {
-  filename         = data.archive_file.example.output_path
-  function_name    = "example_lambda_function"
-  role             = aws_iam_role.example.arn
-  handler          = "index.handler"
-  source_code_hash = data.archive_file.example.output_base64sha256
-
-  runtime = "nodejs24.x"
-
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_basic_execution
-  ]
-
-  environment {
-    variables = {
-      ENVIRONMENT = "production"
-      LOG_LEVEL   = "info"
-    }
-  }
-
-  tags = {
-    Environment = "production"
-    Application = "example"
-  }
+  bucket_name = var.bucket_name
+  environment = "production"
 }
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0f918f7e67a3323f0"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "my-tf-test-s3bucket-for-productions"
+}
